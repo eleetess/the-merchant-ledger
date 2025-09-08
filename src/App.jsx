184f34bec56b8f006dea0da2d479c2ad229f141d
eleetess <ericaleetesseo@gmai.com>
2025-09-08 06:47:12 -0500

    basic set up

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.scss";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <header>
+        <h1>The Merchant Ledger</h1>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
